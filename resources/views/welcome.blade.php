c750497e79401711919269a270a76d8e5f6ec533
xiuhuai <363694485@qq.com>
2017-02-07 15:00:47 +0800

    null
    null

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -83,7 +83,7 @@
                 </div>
 
                 <div class="links">
-                    <a href="http://localhost/article">article</a>
+                    <a href="http://localhost/article">前台文章</a>  <a href="http://localhost/user">用户中心</a><a href="http://localhost/admin">管理后台</a>
                  
                 </div>
             </div>
